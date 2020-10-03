767fa5127c00789fb492f8455bc81c20cf5239fc
hhxdragon <gygud98@naver.com>
2020-10-03 12:28:17 +0900

    header component 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import styled, {createGlobalStyle} from "styled-components";
+import Header from "./components/Header";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    box-sizing: border-box;
+    text-decoration: none;
+  }
+  
+`;
 
 function App() {
+
   return (
-      <div>
-        app
-      </div>
+      <Container>
+          <GlobalStyle/>
+        <Header/>
+      </Container>
   )
 }
+const Container = styled.div`
+
+`;
 
 export default App;
