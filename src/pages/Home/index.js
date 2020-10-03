fbde1d88094448e91afe6f9cbd3038691a640751
hhxdragon <gygud98@naver.com>
2020-10-03 13:15:39 +0900

    router, pages 구조의 추가

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function Home() {
+
+    return (
+        <Container>
+            Home
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+export default Home;
---
+++ src/pages/Users/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function Users() {
+
+    return (
+        <Container>
+            Users
+        </Container>
+    )
+}
+
+const Container = styled.div`
+
+`;
+
+export default Users;
