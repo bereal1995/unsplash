863095099abfc241982fc114da48c942ce8fec2a
HH <gygud98@naver.com>
2021-01-30 11:59:53 +0900

    111

--- src/view/containers/HomeContainer/Visual.js
+++ src/view/containers/HomeContainer/Visual.js
@@ -2,7 +2,6 @@ import React from 'react';
 import styled from "styled-components";
 import SearchBox from "../../components/SearchBox";
 import {PageContainer} from "../../components/Layout/Styled";
-import TrendKeyword from "../../components/SearchBox/TrendKeyword";
 
 function Visual({visualPhoto}) {
 
